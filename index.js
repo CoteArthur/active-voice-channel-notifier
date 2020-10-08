078638f99e50f067b196f1bd051cd54f0ec2b71d
CoteArthur <57949230+CoteArthur@users.noreply.github.com>
2020-10-09 00:12:31 +0200

    now creates the text channel meant for notifications

--- index.js
+++ index.js
@@ -5,20 +5,31 @@ const client = new Discord.Client();
 let voiceChannel = undefined;
 let textChannel = undefined;
 
+//TODO handle permissions of textChannel and handle proper voiceChannel fetching
+
 client.once('ready', () => {
     console.log('Ready!');
 
-    client.channels.fetch('663505721236652046')
-    .then(channel => voiceChannel = channel)
-    .catch(console.error);
+    // client.channels.fetch('663505721236652046')
+    // .then(channel => voiceChannel = channel)
+    // .catch(console.error);
 
-    client.channels.fetch('763781261386448986')
-    .then(channel => textChannel = channel)
-    .catch(console.error);
+    // client.channels.fetch('763781261386448986')
+    // .then(channel => textChannel = channel)
+    // .catch(console.error);
 });
 
+client.on("guildCreate", guild => {
+    guild.channels.create('bot-notifications')
+    .then(channel => {
+        textChannel = channel;
+        textChannel.send('beep boop');
+    })
+    .catch(console.error);
+})
+
 client.on('voiceStateUpdate', (oldState, newState) => {
-    if (!voiceChannel || !textChannel) return console.log('Channels not set properly, this is probably an ID error');
+    if (!voiceChannel || !textChannel) return console.log('Channels not set properly');
 
     if(oldState.channel === null && newState.channel !== null) {
 
